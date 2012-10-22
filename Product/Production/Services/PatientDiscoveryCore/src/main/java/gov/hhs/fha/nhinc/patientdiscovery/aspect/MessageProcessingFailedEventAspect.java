c391d4e0a6a59249a720c3c3aae2047c63da2841
Brian Humphrey <bhumphrey@5amsolutions.com>
2012-10-22 09:20:38 -0400

    save point; pd core unit test failing

---
+++ Product/Production/Services/PatientDiscoveryCore/src/main/java/gov/hhs/fha/nhinc/patientdiscovery/aspect/MessageProcessingFailedEventAspect.java
@@ -0,0 +1,9 @@
+package gov.hhs.fha.nhinc.patientdiscovery.aspect;
+
+public class MessageProcessingFailedEventAspect extends PatientDiscoveryEventAspect {
+
+    public MessageProcessingFailedEventAspect() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
